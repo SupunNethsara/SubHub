af3f78a42643d7f203438aaf7caff070e07b6d82
Supun Nethsara <supunmax663@gmail.com>
2025-09-21 05:10:23 -0700

    Add logic to  SubscriptionController.php

--- app/Http/Controllers/SubscriptionController.php
+++ app/Http/Controllers/SubscriptionController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\SubscriptionRequest;
 use App\Models\Subscription;
 use Illuminate\Http\Request;
 use mysql_xdevapi\Exception;
@@ -27,14 +28,21 @@ class SubscriptionController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(SubscriptionRequest $request)
     {
-
+        $validated = $request->validated();
         try{
-
+            $subscription = Subscription::create($validated);
+            return response()->json([
+                'success' => true,
+                'subscription' => $subscription
+            ], 201);
         }
         catch (\Exception $exception) {
-
+            return  response()->json([
+               'success'=>false ,
+                'message' => $exception->getMessage()
+            ] , 500);
         }
     }
 
