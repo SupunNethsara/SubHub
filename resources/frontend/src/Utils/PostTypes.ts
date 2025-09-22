aed3c7dc15404b6be2ff85745b3c2881a29d938e
Supun Nethsara <supunmax663@gmail.com>
2025-09-22 04:24:36 -0700

    Fetch post data by using website_id and it's mapping to ui

---
+++ resources/frontend/src/Utils/PostTypes.ts
@@ -0,0 +1,11 @@
+
+export  interface Post {
+    id: string;
+    title: string;
+    content: string;
+    website_id: string;
+    image: string;
+    created_at: string;
+    updated_at: string;
+    image_url: string;
+}
